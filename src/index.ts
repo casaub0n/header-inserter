8b0f15b99dc856ae1a1fcfa305aaba45ca47eb8e
Casaubon <netzach.yoshida@gmail.com>
2020-07-31 19:58:18 +1200

    Add feature for inserting comment of userscript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('hello')
+import path from 'path'
+import fs from 'fs'
+import { insertHeader } from './insertHeader'
+
+type UserScriptHeader = {
+  name: string,
+  namespace: string,
+  version: string
+  description: string
+  authoer: string,
+  match: string,
+  grant: string
+}
+
+console.log(process.cwd())
+const configPath = path.join(process.cwd(), 'package.json')
+console.log(configPath)
+const configJson: UserScriptHeader = JSON.parse(fs.readFileSync(configPath, 'utf-8'))
+const userScriptHeader = "// ==UserScript==\n// @name         " + configJson.name + "\n// @namespace    " + configJson.namespace + "\n// @version      "
+  + configJson.version + "\n// @description  " + configJson.description + "\n// @author       " + configJson.authoer + "\n// @match        "
+  + configJson.match + "\n// @grant        " + configJson.grant + "\n// ==/UserScript==\n\n"
+
+console.log(`config json: \n${userScriptHeader}`)
+const files = process.argv.slice(2);
+files.map((f) => insertHeader(f, userScriptHeader))
